67538ac48edeed652a91eb40b13248bcf9a5b546
Serkan Dayıcık <x0nim@windowslive.com>
2017-07-07 17:00:09 +0300

    Deprecated Moongose connection fix
    Deprecated Mongoose connection function changed
    ref:
    http://mongoosejs.com/docs/connections.html#use-mongo-client

--- src/db.js
+++ src/db.js
@@ -6,6 +6,9 @@ const db = mongoose.connection
 
 export default callback => {
 	// connect to a database if needed, then pass it to `callback`:
-	mongoose.connect(config.db)
+	mongoose.connect(config.db, {
+	// http://mongoosejs.com/docs/connections.html#use-mongo-client
+		useMongoClient: true,
+	});
 	callback(db);
 }
